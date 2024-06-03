4b6424a6b87740464e22d9fcb852a8dec325c516
sadekcric4 <sadekcric4@gmail.com>
2024-06-03 09:34:41 +0600

    create page implement done

--- src/Dashboard/TaskCreator/AddNewTask.jsx
+++ src/Dashboard/TaskCreator/AddNewTask.jsx
@@ -1,16 +1,96 @@
-// import { useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-// import DatePicker from "react-datepicker";
+import useGetCoin from "../../Hooks/useGetCoin";
+import Swal from "sweetalert2";
+import useAuth from "../../Hooks/useAuth";
+import useAxiosSecure from "./../../Hooks/useAxiosSecure";
+import useUploadImage from "./../../Hooks/useUploadImage";
 
 const AddNewTask = () => {
-  // const [startDate, setStartDate] = useState(new Date());
+  const [coin, isLoading] = useGetCoin();
+  const [image, setImage] = useState(null);
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const uploadImage = useUploadImage();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const handleAddProduct = () => {};
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="relative">
+          <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-customSecondary"></div>
+          <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-customPrimary animate-spin"></div>
+        </div>
+      </div>
+    );
+  }
+
+  const handleImage = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
+
+  const handleAddProduct = async (data) => {
+    const expireDate = new Date(data.date);
+    const taskQuantity = parseInt(data.quantity);
+    const taskPayable = parseInt(data.payable);
+    const totalPayable = taskQuantity * taskPayable;
+
+    if (coin <= totalPayable) {
+      return Swal.fire({
+        icon: "info",
+        title: "Insufficient Balance!",
+        text: "Please Resurge and Try Again",
+        showConfirmButton: false,
+        timer: 3000,
+      });
+    }
+
+    const imageFile = { image: data.image[0] };
+    const uploadingImage = await uploadImage(imageFile);
+    const photo = uploadingImage?.data?.data?.display_url;
+
+    const info = {
+      title: data.title,
+      details: data.details,
+      quantity: data.quantity,
+      payable: data.payable,
+      completionDate: expireDate,
+      submissionInfo: data.info,
+      taskImage: photo,
+      creator: user.displayName,
+      email: user.email,
+      createTime: new Date(),
+    };
+
+    axiosSecure
+      .post("/add-task", info)
+      .then((res) => {
+        if (res?.data?.insertedId) {
+          Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: "Your Task Successfully Added!",
+            showConfirmButton: false,
+            timer: 3000,
+          });
+        }
+      })
+      .catch((err) => {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: err.message,
+          showConfirmButton: false,
+          timer: 3000,
+        });
+      });
+  };
 
   return (
     <section>
@@ -26,7 +106,7 @@ const AddNewTask = () => {
           }}
         >
           <div className="lg:w-1/2 mx-auto m-4 bg-opacity-70 p-3 lg:p-5 bg-[#fff] rounded-2xl shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)]">
-            <h2 className="text-customPrimary text-xl lg:text-3xl font-bold text-center my-5">Add Product</h2>
+            <h2 className="text-customPrimary text-xl lg:text-3xl font-bold text-center my-5">Add New Task</h2>
             {/* add product Form */}
             <form onSubmit={handleSubmit(handleAddProduct)}>
               {/* task title  */}
@@ -46,6 +126,7 @@ const AddNewTask = () => {
               <div className="flex gap-3 mt-3 items-center">
                 <div className="flex-1">
                   <div className="relative flex flex-col items-center">
+                    <p className="text-sm absolute bottom-3 left-28">Completion Time</p>
                     <input
                       type="date"
                       {...register("date", { required: true })}
@@ -85,12 +166,13 @@ const AddNewTask = () => {
                 <div className="flex-1">
                   <div className="relative">
                     <div className="absolute  top-0  left-0 w-full bg-transparent  py-3 border-b border-[#333] text-[#333] text-sm px-2">
-                      Enter Photo
+                      {image ? `${image.name.length > 20 ? "..." + image.name.slice(-20) : image.name}` : "Enter Photo"}
                     </div>
                     <button className="opacity-0 z-10">
                       <input
                         {...register("image", { required: true })}
                         type="file"
+                        onChange={handleImage}
                         required
                         className=" block w-full px-10 cursor-pointer py-2"
                       />
---
+++ src/Hooks/useGetCoin.jsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import useAxiosPublic from "./useAxiosPublic";
+
+const useGetCoin = () => {
+  const { user } = useAuth();
+  const axiosPublic = useAxiosPublic();
+
+  const { data: coin, isLoading } = useQuery({
+    queryKey: ["coin", user?.email],
+    queryFn: async () => {
+      const findUser = await axiosPublic.get(`/users/${user.email}`);
+      const UserRole = findUser.data.coin;
+      return UserRole;
+    },
+  });
+
+  return [coin, isLoading];
+};
+
+export default useGetCoin;
